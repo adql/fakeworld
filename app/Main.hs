b507c02a94758059347a167ec6ab6fdbdbb5984d
Amir Dekel <6893125+adql@users.noreply.github.com>
2022-10-23 21:20:24 +0200

    Create front page mockup with placeholders

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,66 @@
 module Main where
 
+import Brick
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Center as C
+import Data.Function ((&))
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = simpleMain homePage
+
+type Name = () --placeholder
+
+homePage :: Widget Name
+homePage = navigation
+           <=>
+           banner
+           <=>
+           content
+           <=>
+           footer
+
+navigation :: Widget Name
+navigation =
+  padTopBottom 1 $
+  limitWidthAndCenter $
+  (str "conduit" & padRight Max) <+>
+  str "Home   Sign in   Sign up"  
+
+banner :: Widget Name
+banner =
+  vBox [ B.hBorder &
+         padBottom (Pad 2)
+       , C.hCenter $ str "conduit" &
+         padBottom (Pad 1)
+       , C.hCenter $ str "a place to share your knowledge"
+       , padTop (Pad 2) $
+         B.hBorder
+       ]
+
+content :: Widget Name
+content =
+  limitWidthAndCenter $
+  feed <+> hLimitPercent 25 tags
+
+feed :: Widget Name
+feed =
+  B.border $
+  C.hCenter (str "<global feed>")
+  <=> fill ' '
+
+tags :: Widget Name
+tags =
+  B.border $
+  vLimit 10 $
+  C.hCenter (str "<popular tags>")
+  <=> fill ' '
+
+footer :: Widget Name
+footer =
+  padTop (Pad 1) $
+  limitWidthAndCenter $
+  str "conduit. An interactive learning project from Thinkster. Code & design licensed under MIT. Implemented by Amir Dekel."
+  & padRight Max
+
+limitWidthAndCenter :: Widget n -> Widget n
+limitWidthAndCenter = C.hCenter . hLimit 120
